cefdfac2b720a2fe27970d239666c5e67fd7ce1f
Adonias Silva <adoniaslg@gmail.com>
2025-10-15 15:27:53 -0300

    Update PaginationResult.java

--- src/main/java/br/com/benomous/common/PaginationResult.java
+++ src/main/java/br/com/benomous/common/PaginationResult.java
@@ -1,5 +1,6 @@
 package br.com.benomous.common;
 
+import java.util.List;
 
 public record PaginationResult<Object>(int page, int size,
                                        int numberOfPages, List<Object> data) {
